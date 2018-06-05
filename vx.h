25ce21dd487690fb092e32fae971a74900ba7ceb
chuangix <chuangix@qq.com>
2018-06-05 21:03:56 +0800

    Add vx.h

---
+++ vx.h
@@ -0,0 +1,18 @@
+#ifndef VX_H_
+#define VX_H_
+
+#include "vxWorks.h"
+
+#include "inetLib.h"
+#include "intLib.h"
+#include "iv.h"
+#include "logLib.h"
+#include "msgQLib.h"
+#include "sysLib.h"
+#include "sockLib.h"
+#include "stdioLib.h"
+#include "sysLib.h"
+#include "taskLib.h"
+#include "tickLib.h"
+
+#endif /* VX_H_ */
