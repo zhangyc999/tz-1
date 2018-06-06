34a94ecdd57ce72bc64aa3461b0eb0555c9c9f5d
chuangix <chuangix@qq.com>
2018-06-07 02:57:28 +0800

    Add msg_dbg into tz.c

--- tz.c
+++ tz.c
@@ -16,6 +16,7 @@ MSG_Q_ID msg_can[2];
 MSG_Q_ID msg_main;
 MSG_Q_ID msg_pwr;
 MSG_Q_ID msg_swh;
+MSG_Q_ID msg_dbg;
 
 void tz(void)
 {
@@ -26,6 +27,7 @@ void tz(void)
         msg_main = msgQCreate(32, sizeof(struct main), MSG_Q_FIFO);
         msg_pwr = msgQCreate(4, sizeof(struct frame_can), MSG_Q_FIFO);
         msg_swh = msgQCreate(4, sizeof(struct frame_can), MSG_Q_FIFO);
+        msg_dbg = msgQCreate(128, sizeof(struct frame_can), MSG_Q_FIFO);
         taskSpawn("MAIN", 90, VX_FP_TASK, 20000, (FUNCPTR)t_main, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         taskSpawn("UDP", 90, VX_FP_TASK, 20000, (FUNCPTR)t_udp, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         taskSpawn("CAN", 90, VX_FP_TASK, 20000, (FUNCPTR)t_can, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
